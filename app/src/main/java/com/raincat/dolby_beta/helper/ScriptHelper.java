97760fe7ec418dcd2f024efa11c93eb5e7874d8a
wx1141235171 <wx1141235171@outlook.com>
2024-06-21 12:45:03 +0800

    Update ScriptHelper.java

--- app/src/main/java/com/raincat/dolby_beta/helper/ScriptHelper.java
+++ app/src/main/java/com/raincat/dolby_beta/helper/ScriptHelper.java
@@ -90,7 +90,7 @@ public class ScriptHelper {
     }
 
     public static void startScript() {
-        String script = String.format("export ENABLE_FLAC=%s&&export MIN_BR=%s&&export QQ_COOKIE=\"%s\"&&export KUWO_PROXY=\"%s\"&&export MIGU_COOKIE=\"%s\"&&libnode.so app.js -a 127.0.0.1 -o %s -p %s",
+        String script = String.format("export ENABLE_FLAC=%s&&export MIN_BR=%s&&export QQ_COOKIE=\"%s\"&&export KUWO_PROXY=%s&&export MIGU_COOKIE=\"%s\"&&libnode.so app.js -a 127.0.0.1 -o %s -p %s",
                 SettingHelper.getInstance().getSetting(SettingHelper.proxy_flac_key), SettingHelper.getInstance().getSetting(SettingHelper.proxy_priority_key) ? "256000" : "96000",
                 SettingHelper.getInstance().getQqCookie(),SettingHelper.getInstance().getKuwoProxy(),
                 SettingHelper.getInstance().getMiguCookie(),SettingHelper.getInstance().getProxyOriginal(), SettingHelper.getInstance().getProxyPort() + ":" + (SettingHelper.getInstance().getProxyPort() + 1));
